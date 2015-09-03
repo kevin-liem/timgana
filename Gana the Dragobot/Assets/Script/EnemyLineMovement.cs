575609a1b2ecec5c58cdaffb7a72a985bea45a8a
kevin-liem <reeden88@gmail.com>
2015-09-03 21:26:47 +0700

    Player Movement Enemy Movement Add Touch Area Invisible UI

---
+++ Gana the Dragobot/Assets/Script/EnemyLineMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLineMovement : MonoBehaviour {
+
+    private Transform enemyTransform;
+    public float speed = 1f;
+
+    private float direction = 1f;
+    public bool reverseDirection;
+
+    void Start()
+    {
+        enemyTransform = GetComponent<Transform>();
+        if (reverseDirection)
+        {
+            direction = -1f;
+        }
+    }
+
+    void Update()
+    {
+        enemyTransform.Translate(direction * -Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ Gana the Dragobot/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    private Transform playerTransform;
+    public float speed = 1f;
+
+    private float direction = 1f;
+
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        playerTransform.Translate(direction * Vector3.right * Time.deltaTime * speed);
+    }
+
+    public void ReverseDirection()
+    {
+        direction *= -1f;
+    }
+}
